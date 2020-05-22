b893e184cf0e64f2160926fa653e84522c3cb34d
Curzuv <alexandr.chebatarev@gmail.com>
2020-05-22 15:45:50 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import psutil
+import time
+import os
+import re
+import subprocess
+import pyttsx3
+from nvidia_smi import nvmlInit, NVMLError, nvmlDeviceGetHandleByIndex, nvmlDeviceGetTemperature, NVML_TEMPERATURE_GPU
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[16].id)
+
+def play_audio(temperature):
+    engine.say('Get out, it is gonna blow. Temperature is ' + temperature + " °Celsius")
+    engine.runAndWait()
+
+def sendmessage(message):
+    os.system('notify-send "{}" "{}"'.format("Temperature Warning", message))
+    return
+
+def get_gpu_temp():
+    try:
+        nvmlInit()
+        gpu = nvmlDeviceGetHandleByIndex(0)
+        gpu_temp = nvmlDeviceGetTemperature(gpu, NVML_TEMPERATURE_GPU)
+        return gpu_temp
+    except NVMLError:
+        print("Failed to initialize NVML: ")
+        print("Exiting...")
+
+def temp_check():
+    hot = 0
+    data = psutil.sensors_temperatures()
+    temperatures = re.findall("current=[0-9]{0,3}\.[0-9]", str(data['coretemp']))
+    for i, t in enumerate(temperatures):
+        temperatures[i] = float(t[8:])
+        hot = max(temperatures[i], hot)
+    gpu_temp = get_gpu_temp()
+    if gpu_temp:
+        hot = max(hot, gpu_temp)
+        temperatures.append("gpu: "  + str(gpu_temp))
+    if hot > 80 and hot < 98:
+        sendmessage("Be carefull one of the cpu cores is " + str(hot))
+    elif hot >= 98: 
+        sendmessage("Turn it off, or it is gonna blow, " + str(hot))
+        
+    print(temperatures)
+
+try:
+    while(True):
+        temp_check()
+        time.sleep(60)
+except KeyboardInterrupt:
+    print("Lets stop it boys")
